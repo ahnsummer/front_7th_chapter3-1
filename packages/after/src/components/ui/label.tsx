4e1d4db902e4c7a4679631d8e52f74f2c39142c4
ahnsummer <bazaar9095@gmail.com>
2025-11-25 02:41:39 +0900

    refactor. 중복작성되어 있던 Label 컴포넌트 분리 및 테스트코드 작성

--- packages/after/src/components/ui/label.tsx
+++ packages/after/src/components/ui/label.tsx
@@ -1,12 +1,14 @@
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Label({
   className,
+  children,
+  required = false,
   ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+}: React.ComponentProps<typeof LabelPrimitive.Root> & { required: boolean }) {
   return (
     <LabelPrimitive.Root
       data-slot="label"
@@ -14,9 +16,11 @@ function Label({
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className
       )}
-      {...props}
-    />
-  )
+      {...props}>
+      {children}
+      {required && <span className="text-danger">*</span>}
+    </LabelPrimitive.Root>
+  );
 }
 
-export { Label }
+export { Label };
