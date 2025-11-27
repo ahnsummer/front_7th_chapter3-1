9e2a9718c823cb29f3e7264545197ec95760c9e0
ahnsummer <bazaar9095@gmail.com>
2025-11-27 17:32:08 +0900

    refactor. css파일 정리

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Header } from "@repo/after";
 import { ManagementPage } from "./pages/management-page/ManagementPage";
 import "./index.css";
-import "./styles/components.css";
 
 export const App: React.FC = () => {
   return (
